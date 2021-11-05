ba24aba3cc22f0140d4a207711955d327fcba445
Somya Bhargava <Bhargava.so@husky.neu.edu>
2021-11-04 20:22:41 -0400

    added part 2

--- Day1/main.py
+++ Day1/main.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import pandas as pd
 
@@ -9,3 +8,19 @@ output = np.sum(np.floor(matrix_input / 3) - 2)
 
 # Print the output
 print(output)
+
+
+## part 2
+
+matrix_input = matrix_input.tolist()[0]
+
+total_fuel = 0
+for f in matrix_input:
+    new_fuel = f
+    while True:
+        new_fuel = np.floor(new_fuel/3) - 2
+        if new_fuel > 0:
+            total_fuel += new_fuel
+        else:
+            break
+print(total_fuel)
